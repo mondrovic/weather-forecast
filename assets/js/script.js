4db1397f760197967e1adebc64804a1125d4afa0
Mondrovic <mondrovic1@gmail.com>
2020-09-06 10:21:53 -0700

    added cards and color to UV index

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,13 @@
 var searchInputEl = document.querySelector('#search-btn')
 var searchFormEl = document.querySelector('#search-form')
 var currentDayEl = document.querySelector('#current-day')
+var fiveDayEl = document.querySelector('#five-day')
+var cardContainerEl = document.querySelector('#card-container')
+
 var currentTime = moment().format('MMM Do, YYYY')
+
+var lat = 0
+var lon = 0
 
 // Handler for when user clicks submit button
 var formSubmitHandler = function(){
@@ -23,16 +29,31 @@ var formSubmitHandler = function(){
 };
 
 
-// Gets API data from openweathermap for current day
+// Gets API data from openweathermap for current day and forecast
 var getWeather = function(location){
-    // format the open weather url
+    // format the open weather url for current
     var apiCurrent = 'https://api.openweathermap.org/data/2.5/weather?q=' + location + '&units=imperial&appid=23ccf9e8695292de833093bcb9dac5b7'
+
     // makes request to URL with fetch
     fetch(apiCurrent).then(function(response){
         // makes sure response status is ok
         if(response.ok){
             response.json().then(function(data){
+                // saves variables to use in other fetch calls
+                lat = data.coord.lat;
+                lon = data.coord.lon;
                 displayCurrent(data);
+                // runs fetch command for forecast -- have to run the previous fetch command first because have no way to input lat/lon without it
+                var apiForecast = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&exclude=current,minutely,hourly&units=imperial&appid=23ccf9e8695292de833093bcb9dac5b7'
+                fetch(apiForecast).then(function(response){
+                    if(response.ok){
+                        response.json().then(function(forecast){
+                            displayForecast(forecast);
+                        });
+                    } else{
+                        alert('Error: ' + response.statusText);
+                    }
+                });                
             });
         } else{
             alert('Error: ' + response.statusText);
@@ -42,17 +63,11 @@ var getWeather = function(location){
     .catch(function(error){
         alert('Unable to connect to Open Weather');
     });
-    
-
 };
 
 // displays the current date's information
 var displayCurrent = function(data){
-    currentDayEl.textContent = ''
-
-    // sets variables for lat/lon        
-    var lat = data.coord.lat;
-    var lon = data.coord.lon;
+    currentDayEl.textContent = '';
     
     // places name of location and time
     var cityDisplayEl = document.createElement('h4')
@@ -79,12 +94,35 @@ var displayCurrent = function(data){
     uvFetch(lat, lon)
 }
 
-var display5Day = function(data){
-    console.log(data);
+// ---- 5 Day Forecast Start
+var displayForecast = function(forecast){
+    console.dir(forecast);
+    // empties out cards
+    cardContainerEl.textContent = '';
+
+    // creates cards for next 5 days
+    for(i = 0; i < 5; i++){
+        var dailyCard = document.createElement('div');
+        dailyCard.classList = 'card bg-primary text-light m-2';
+
+        var dailyDate = document.createElement('h4');
+        var newDate = moment(currentTime).clone().add(1, 'd');
+        dailyDate.textContent = newDate;
+        dailyCard.appendChild(dailyDate);
+
+        var dailyTempEl = document.createElement('p');
+        dailyTempEl.textContent = 'Temperature: ' + forecast.daily[i].temp.max + '\u00B0F';
+        dailyCard.appendChild(dailyTempEl);
+
+        var dailyHumidityEl = document.createElement('p');
+        dailyHumidityEl.textContent = 'Humidity: ' + forecast.daily[i].humidity + '%';
+        dailyCard.appendChild(dailyHumidityEl);
+
+        cardContainerEl.appendChild(dailyCard);
+    }
 }
 
-// ---- UV functions start. UV has different API call than regular weather
-    
+// ---- UV functions start. UV has different API call than regular weather    
 // displays UV information to container*/
 var displayUv = function(uvData){
     var uvEl = document.createElement('p');
@@ -93,8 +131,18 @@ var displayUv = function(uvData){
 
     var uvRisk = document.createElement('span');
     uvRisk.textContent = uvData.value;
-    // if(uvData.value)
-    // uvRisk.classList = 'okay';
+
+    if(uvData.value <= 2){
+        uvRisk.classList = 'low';
+    }else if(uvData.value >2 && uvData.value <=5){
+        uvRisk.classList = 'moderate';
+    }else if(uvData.value >5 && uvData.value <=7){
+        uvRisk.classList = 'high';
+    }else if(uvData.value >7 && uvData.value <=10){
+        uvRisk.classList = 'very-high';
+    }else{
+        uvRisk.classList = 'extreme';
+    }
 
     uvEl.appendChild(uvRisk);
     currentDayEl.appendChild(uvEl);
